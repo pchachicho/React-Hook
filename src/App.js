131b7dfa157074f3c9a2c5cadb465a4367b57d82
Patrick hachicho <Hachichopatrick@gmail.com>
2022-10-22 13:07:42 -0400

    way of increasing button count

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [currentState, updatedState] = useState(4);
+
+  function decreaseCount() {
+    updatedState(currentState - 1);
+  }
+  function increaseCount() {
+    updatedState(currentState + 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={decreaseCount}>-</button>
+      <span>{currentState}</span>
+      <button onClick={increaseCount}>+</button>
+    </>
   );
 }
 
